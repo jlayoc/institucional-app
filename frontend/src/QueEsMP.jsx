36ae6dd1537c6f0dfa03919cc58a3f04e5a5e3de
jose_luis <41703528+jlayoc@users.noreply.github.com>
2025-11-20 21:14:29 -0600

    Create QueEsMP.jsx

---
+++ frontend/src/QueEsMP.jsx
@@ -0,0 +1,12 @@
+export default function QueEsMP() {
+  return (
+    <div>
+      <h2>¿Qué es el Ministerio Público?</h2>
+      <p>
+        El Ministerio Público es la institución encargada de la persecución penal,
+        la dirección funcional de la investigación de los delitos y la protección
+        de los intereses de la sociedad, actuando con objetividad e imparcialidad.
+      </p>
+    </div>
+  );
+}
